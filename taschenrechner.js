c76d814f2ac78bb0dd5965d9c9e4da87c961cbba
Taner Aksoy <aksoytaner123@gmail.com>
2023-10-12 18:20:45 +0200

    fix: fixed everything

--- taschenrechner.js
+++ taschenrechner.js
@@ -61,46 +61,47 @@ document.addEventListener("DOMContentLoaded", function(){
         input = input.replace(/x/g,"*")
 });
 */
+
+
 document.addEventListener("DOMContentLoaded", function(){
-    const calculator = document.querySelector('.calc')
-    const keys = calculator.querySelector('.calc_keys')
-});
 
+    const calculator = document.querySelector('.calculator')
+    const keys = calculator.querySelector('.calculator__keys')
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')){
+    keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+    
         const key = e.target
         const action = key.dataset.action
-        const keyContent = key.textContent
-        const displayedNum = display.textContent
+        
+        if (!action) {
+            console.log('number key!')
+          }
+
+        if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+        ) {
+        console.log('operator key!')
+        }
+
+        if (action === 'decimal') {
+            console.log('decimal key!')
+          }
+          
+          if (action === 'clear') {
+            console.log('clear key!')
+          }
+          
+          if (action === 'calculate') {
+            console.log('equal key!')
+          }
     }
 })
 
-const key = e.target
-const action = key.dataset.action
+});
 
 
-if(!action) {
-    console.log('number key!')
-}
 
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    console.log('operator key!')
-}
-
-if (action === 'decimal') {
-    console.log('decimal key!')   
-}
-
-if (action === 'clear') {
-    console.log('clear key!')
-}
-
-if (action === 'calculate') {
-    console.log('equal key!')
-}
